c1cc091f103c0b2d093caab02e746b0a8769505d
lordjustine <57934586+lordjustine@users.noreply.github.com>
2019-12-20 04:12:21 +0800

    send-verification-code.sh
    curl -X GET "https://api.nexmo.com/verify/json?&api_key=$NEXMO_API_KEY&api_secret=$NEXMO_API_SECRET&number=$RECIPIENT_NUMBER&brand=AcmeInc"

--- verify/send-verification-code.sh
+++ verify/send-verification-code.sh
@@ -2,3 +2,5 @@
 source "../config.sh"
 
 curl -X GET "https://api.nexmo.com/verify/json?&api_key=$NEXMO_API_KEY&api_secret=$NEXMO_API_SECRET&number=$RECIPIENT_NUMBER&brand=AcmeInc"
+
+curl -X GET "https://api.nexmo.com/verify/json?&api_key=$NEXMO_API_KEY&api_secret=$NEXMO_API_SECRET&number=$RECIPIENT_NUMBER&brand=AcmeInc"
